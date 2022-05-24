1b4414660ecca2f5202f02fa070a321dca07a968
Giovanna Antonucci <97487819+gioantonucci@users.noreply.github.com>
2022-05-24 11:21:03 -0300

    initial commit

---
+++ api/src/models/Dog.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('dog', {
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4
+      },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    height_min: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    height_max: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    weight_min: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    weight_max: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    years: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      userCreated: {
+        type: DataTypes.BOOLEAN,
+        default: true,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+  },
+});
+};
