d9ee94e43200dbb90302c86618b64c2f1879e510
Giovanna Antonucci <97487819+gioantonucci@users.noreply.github.com>
2022-06-09 20:44:40 -0300

    link fix

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import github from '../img/github.png'
-import linkedin from '../img/linkedin.png'
-import s from '../styles/About.module.css'
+import github from "../img/github.png";
+import linkedin from "../img/linkedin.png";
+import s from "../styles/About.module.css";
 
-
-export default function About (){
- return (
+export default function About() {
+  return (
     <div className={s.container}>
-    <div className={s.about}>
-    <h1 className={s.text}>Hello there!</h1>
-    <h3 className={s.text}> Im Giovanna, a Full Stack Developer! <br/>
-    I made this page as an Individual Project from Soy Henry Bootcamp! <br/>
-    </h3>
-    <h3 className={s.text}>Let's connect!</h3>
-    
-    <Link to="https://github.com/gioantonucci">
+      <div className={s.about}>
+        <h1 className={s.text}>Hello there!</h1>
+        <h3 className={s.text}>
+          {" "}
+          Im Giovanna, a Full Stack Developer! <br />
+          I made this page as an Individual Project from Soy Henry Bootcamp!{" "}
+          <br />
+        </h3>
+        <h3 className={s.text}>Let's connect!</h3>
+
+        <a href="https://github.com/gioantonucci">
+          {" "}
           <img
             className={s.img}
             id="about"
@@ -24,8 +27,9 @@ export default function About (){
             height="50"
             alt="img not found"
           />
-        </Link>
-        <Link to="https://www.linkedin.com/in/giovanna-antonucci/">
+        </a>
+
+        <a href="https://www.linkedin.com/in/giovanna-antonucci/">
           <img
             className={s.img}
             id="linkedin"
@@ -34,13 +38,13 @@ export default function About (){
             height="50"
             alt="img not found"
           />
-        </Link>
-        </div>
-   <div className={s.btnDiv}>
-   <NavLink to="/home">
-   <button className={s.btn}> Back to home</button>
-   </NavLink>
-   </div>
-</div>
- )
+        </a>
+      </div>
+      <div className={s.btnDiv}>
+        <NavLink to="/home">
+          <button className={s.btn}> Back to home</button>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
