f7d6d923677845b6b181c5e6139d243baefad60f
gioantonucci <gioantonucci@hotmail.com>
2023-01-17 18:03:57 -0300

    deploy railway

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
-
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const port = process.env.PORT
+const port = process.env.PORT | 3001
+
+
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
