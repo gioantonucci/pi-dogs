5b9b7b7ff7c0209aa461c641961e4687c057dc0a
gioantonucci <gioantonucci@hotmail.com>
2023-03-13 15:50:50 -0300

    host

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const {PORT} = process.env
+const {DB_PORT} = process.env
 
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+  server.listen(DB_PORT, () => {
+    console.log(`%s listening at ${DB_PORT}`); // eslint-disable-line no-console
   });
 });
