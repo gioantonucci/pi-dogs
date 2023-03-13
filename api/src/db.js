427d1be42d35fa5ebcb8e0e9507422f560d1a619
gioantonucci <gioantonucci@hotmail.com>
2023-03-13 20:40:18 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 
-const { REACT_APP_DB_DEPLOY } =
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, DB_DEPLOY } =
   process.env;
 
-const sequelize = new Sequelize(REACT_APP_DB_DEPLOY, {
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
