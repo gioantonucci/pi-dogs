27499c2c66a8fa2c2d2c343eecda55e5b7516f5a
gioantonucci <gioantonucci@hotmail.com>
2023-03-13 16:25:49 -0300

    host

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config()
-const {PORT} = process.env
+
+const {PORT} = process.env.PORT || 3001
 
 
 // Syncing all the models at once.
